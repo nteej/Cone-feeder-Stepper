6366918905d0b948a335171ba6d9e4054f3cb197
Tharanga <nteeje@gmail.com>
2023-04-28 05:19:50 +0400

    Most Optimized base code

--- Cone-feeder-Stepper.ino
+++ Cone-feeder-Stepper.ino
@@ -1,39 +1,26 @@
 #include <AFMotor.h>
-
-// Number of steps per output rotation
-// Change this as per your motor's specification
 const int stepsPerRevolution = 200;
-//number of pulses received by the stepper 
-volatile unsigned int pulseCount = 0;
-// connect motor to port #2 (M3 and M4)
 AF_Stepper motor(stepsPerRevolution, 1);
-int stepper_rpm = 22;
-int step_threshold = stepper_rpm-3
-void countPulses() {
-  pulseCount++;
-}
-
 void setup() {
-  
-  Serial.begin(9600);
-  Serial.println("Coconut Oil Expeller Feeder");
-
-  motor.setSpeed(stepper_rpm);  // 10 rpm   
-  attachInterrupt(digitalPinToInterrupt(2), countPulses, RISING);
-
+  motor.setSpeed(22);
 }
-
 void loop() {
-  pulseCount = 0; // reset pulse count
-  delay(1000); // wait one second
-  float rpm = (float)pulseCount / 200.0 * 60.0; // calculate RPM
-  Serial.print("RPM: ");
-  Serial.println(rpm);
- // Serial.println("Single coil steps");
- if(rpm > step_threshold){
-  motor.step(100, FORWARD, DOUBLE); 
-  }else{
-    motor.step(25, FORWARD, SINGLE);
+  static const uint8_t STEPS_PER_CYCLE = 4; // Use a multiple of 4 for smoother motion
+  static const uint16_t STEPS_PER_SEC = 440; // 22 RPM * 200 steps / 60 seconds
+  static const uint16_t STEPS_PER_LOOP = STEPS_PER_SEC / 60; // 1/60th of a second per loop
+  static uint8_t stepCount = 0;
+  static uint32_t lastStepTime = 0;
+  uint32_t currentTime = millis();
+  
+  if ((currentTime - lastStepTime) >= (1000 / STEPS_PER_SEC)) {
+    if (stepCount < STEPS_PER_CYCLE) {
+      motor.onestep(FORWARD, SINGLE);
+      stepCount++;
+    }
+    lastStepTime = currentTime;
   }
- // motor.step(100, BACKWARD, SINGLE); 
+  
+  if (stepCount >= STEPS_PER_CYCLE) {
+    stepCount = 0;
+  }
 }
